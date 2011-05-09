11d2950b3e118c50c35aedb87e887cb3d0cc1f41
Danny Hakim <danny.hakim@slytics.com>
2011-05-09 13:51:41 -0700

    replaced outdated MySQLdb connection.escape function with less troublesome connection.literal

--- sql.py
+++ sql.py
@@ -12,6 +12,6 @@ def formatList(list_object):
 def insertRow(cursor, table, data):
     keys = str(formatList(data.keys()).replace("'",""))
     for key in data:
-        data[key] = cursor.connection.escape(data[key])
+        data[key] = cursor.connection.literal(data[key])
     values = str(formatList(data.values()))
     cursor.execute("insert into  %s%s values%s" % (table, keys, values))
